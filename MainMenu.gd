5602558d9c7397a160f84404a637d1ce3f8577ca
João Cachada <jmcs.cachada@gmail.com>
2019-07-26 20:31:45 +0100

    Fixes #11. Counter for autosaves is now properly checked on boot.

--- MainMenu.gd
+++ MainMenu.gd
@@ -6,7 +6,19 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass;
+
+func _init():
+	## Get the autosave counter on boot.
+	var autoSaveCounter = 0; 
+	var saves = player_variables.get_All_Saves();
+	for string in saves:
+		if string.begins_with("autosavegame"):
+			var tempString = string.replace("autosavegame", "");
+			tempString = tempString.format(".save", "");
+			if tempString as int > autoSaveCounter:
+				autoSaveCounter = tempString as int;
+	player_variables.counter = autoSaveCounter;
 
 # Function that determines what happens when the new game button is pressed.
 # For now, just sends to a new start scene. This should be changed later (check github issue #4).
